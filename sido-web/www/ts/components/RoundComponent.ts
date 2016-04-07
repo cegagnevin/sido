4d0e39b455ace6498a84d3c647d6883d295809f5
Patataa <chariere.Fiedler.cedric@gmail.com>
2016-04-07 10:22:35 +0200

    Add of navigation to customer page

--- sido-web/www/ts/components/RoundComponent.ts
+++ sido-web/www/ts/components/RoundComponent.ts
@@ -6,6 +6,15 @@ import {PoiService} from "../services/PoiService";
 import {Poi} from "../models";
 import {Attribute} from "angular2/core";
 import {CORE_DIRECTIVES} from "angular2/common";
+import {
+    ROUTER_DIRECTIVES,
+    ROUTER_PROVIDERS,
+    RouteConfig,
+    Location
+} from 'angular2/router';
+import {Router} from "angular2/router";
+import {Customer} from "../models";
+
 
 @Component({
     selector: 'round',
@@ -20,11 +29,21 @@ import {CORE_DIRECTIVES} from "angular2/common";
 export class RoundComponent {
     id: string;
     round: Round = new Round();
-    constructor(roundService : RoundService, poiService: PoiService, @Attribute('id') id:string){
+
+    router: Router;
+
+    constructor(router: Router, roundService : RoundService, poiService: PoiService, @Attribute('id') id:string){
         this.id = id;
+        this.router = router;
         roundService.getById(this.id)
             .subscribe((res:Round) => {
                 this.round = res;
             });
+    }
+
+    goToCustomer(element: Customer) {
+        if(element.id !== undefined) {
+            this.router.navigate(['Customer', {'id' : element.id}]);
+        }
     }
 }
