33a407575f2f2af5d9e6904777b88eeb2dc5b899
Mat V <mvincent.it@gmail.com>
2016-04-07 14:25:20 +0200

    Add memo to customer

--- sido-server/src/main/java/com/sopra/hackaton/security/SimpleCORSFilter.java
+++ sido-server/src/main/java/com/sopra/hackaton/security/SimpleCORSFilter.java
@@ -21,7 +21,7 @@ public class SimpleCORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token");
 
